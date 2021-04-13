063259169a998424f9cc7e295e9c622dae070b8b
TAD-1 <82429591+TAD-1@users.noreply.github.com>
2021-04-13 12:17:17 +0800

    Add files via upload

---
+++ 03_personal-info.py
@@ -0,0 +1,6 @@
+fName = input("What is your first name? ")
+lName = input("What is your last name? ")
+loc = input("What is your location? ")
+age = input("What is your age? ")
+space = "  "
+print("Hi"  + space + fName  + space + lName + "!" + space + "Your" + space + "location" + space + "is" + space +  loc + space + "and" + space + " you" + space + "are" + space +  age + space +  "years" + space + "old." )
